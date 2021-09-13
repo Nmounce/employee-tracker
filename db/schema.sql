b9e3675ed5a57ac7580deceabd69bb3f60d9c928
NikkiMounce <nikkimounce@gmail.com>
2021-09-13 11:36:49 -0700

    installed packages and started schema

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS roster_db;
+
+CREATE DATABASE roster_db;
+
+USE roster_db;
+
+CREATE TABLE department (
+    dept_id INT NOT NULL PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    role_id INT NOT NULL PRIMARY KEY,
+    role_id INT,
+    role_title VARCHAR(30) NOT NULL,
+    role_salary DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES department(dept_id) ON DELETE
+    SET
+        NULL
+);
+
+CREATE TABLE employee (
+    emp_id INT NOT NULL AUTO_INCRIMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES department(role_id) 
+    SET
+        NULL
+);
---
+++ server.js
@@ -0,0 +1,3 @@
+const app = express();
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
