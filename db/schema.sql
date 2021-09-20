00d6cf971d32d59d10b71b444d03f21f5398c377
NikkiMounce <nikkimounce@gmail.com>
2021-09-20 11:51:44 -0700

    reded seeds and schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,24 +3,57 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department (
-    dept_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    dept_name VARCHAR(30) NOT NULL
+    dept_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE emprole (
-    dept_id INT NOT NULL,
-    role_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    role_title VARCHAR(30) NOT NULL,
-    role_salary DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (dept_id) REFERENCES department(dept_id)
+CREATE TABLE role (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    emp_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES emprole(role_id),
-    FOREIGN KEY (manager_id) REFERENCES employee(emp_id)
+    role_id INT UNSIGNED NOT NULL,
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INT UNSIGNED,
+    INDEX man_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
+
+use employees;
+
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+INSERT INTO employee 
+(first_name, last_name, role_id, manager_id)
+VALUES
+    -- ('Mike', 'Chan', 1, ),
+    -- ('Ashley', 'Rodriguez', 1, null),
+    -- ('Kevin', 'Tupik', 2, null),
+    -- ('Kunal', 'Singh', 2, null),
+    -- ('Malia', 'Brown', 3, null),
+    -- ('Sarah', 'Lourd', 3, null),
+    -- ('Tom', 'Allen', 4, null);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,36 +1,33 @@
-use roster_db;
+SELECT role.id, role.title, role.salary FROM role ORDER BY role.id;
+SELECT role.id, role.title FROM role ORDER BY role.id;
+SELECT * FROM employee;
 
-INSERT INTO
-    department (dept_id, dept_name)
-VALUES
-    (001, 'Sales'),
-    (002, 'Engineering'),
-    (003, 'Finance'),
-    (004, 'Legal'),
-    (005, 'Customer Service');
+SELECT department.id, department.name FROM department ORDER BY department.id;
 
-INSERT INTO
-    emprole (dept_id, role_title, role_salary)
-VALUES
-    (001, 'Salesperson', 80000),
-    (002, 'Lead Engineer', 150000),
-    (002, 'Software Engineer', 120000),
-    (003, 'Account Manager', 160000),
-    (003, 'Accountant', 125000),
-    (004, 'Legal Team Lead', 250000),
-    (004, 'Lawyer', 190000);
+SELECT department.name AS department, role.title, employee.id, employee.first_name, employee.last_name
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY department.name;
 
-INSERT INTO
-    employee (first_name,
-        last_name,
-        role_id,
-        manager_id
-    )
-VALUES
-    ('Mike', 'Chan', 1, null),
-    ('Ashley', 'Rodriguez', 1, null),
-    ('Kevin', 'Tupik', 2, null),
-    ('Kunal', 'Singh', 2, null),
-    ('Malia', 'Brown', 3, null),
-    ('Sarah', 'Lourd', 3, null),
-    ('Tom', 'Allen', 4, null);
+SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, role.title
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id && employee.manager_id != 'NULL')
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY manager;
+
+SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY role.title;
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager_id= employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;
+
+SELECT first_name, last_name, role_id FROM employee WHERE employee.id = 4;
