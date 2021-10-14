49ee309384d304feab9e7b1ba2ccb5e8ab2135cd
NikkiMounce <nikkimounce@gmail.com>
2021-10-14 08:52:01 -0700

    fixed all functionality

--- index.js
+++ index.js
@@ -13,12 +13,18 @@ function startApp() {
             type: 'list',
             message: 'What would you like to do?',
             choices: [
-                "View By Department", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role", "View By Manager", "Exit"
+                "View Employees By Department", "View Employees By Role", "View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role", "View By Manager", "Exit"
             ]
         }]).then(function (answer) {
             switch (answer.menuChoice) {
-                case 'View By Department':
+                case 'View Employees By Department':
                     viewByDepartment();
+                    break;
+                case 'View Employees By Role':
+                    viewByRole();
+                    break;
+                case 'View All Departments':
+                    viewAllDepartments();
                     break;
                 case 'View All Roles':
                     viewAllRoles();
@@ -37,6 +43,7 @@ function startApp() {
                     break;
                 case 'Update Employee Role':
                     updateRole();
+                    break;
                 case 'View By Manager':
                     viewByManager();
                     break;
@@ -47,47 +54,19 @@ function startApp() {
         })
 }
 startApp();
-//     console.log(JSON.stringify(answer.menuChoice));
-//     if (answer.menuChoice === "View All Employees") {
-//         viewAllEmployees();
-//     } else if (answer === "View By Department") {
-//         viewByDepartment();
-//     } else if (answer === "View By Manager") {
-//         viewByManager();
-//     } else if (answer === "Add Employee") {
-//         addEmployee();
-//     } else if (answer === "Remove Employee") {
-//         removeEmployee();
-//     } else if (answer === "Update Role") {
-//         updateRole();
-//     } else if (answer === "View All Roles") {
-//         viewAllRoles();
-//     } else if (answer === "Exit") {
-//         exit();
-//     }
-// })
-// }
-// startApp();
 
-// function viewAllEmployees() {
-// const allEmpQuery = `SELECT employee.id, employee.first_name, employee.last_name, emprole.title, department.name AS department, emprole.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
-//     FROM employee
-//     LEFT JOIN employee manager on manager.id = employee.manager_id
-//     INNER JOIN emprole ON (emprole.id = employee.role_id)
-//     INNER JOIN department ON (department.id = emprole.department_id)
-//     ORDER BY employee.id;`;
-//     let query = db.query(sql, (err, result) => {
-//         if (err) throw err;
-//         console.log('\n');
-//         console.log('VIEW ALL EMPLOYEES');
-//         console.log('\n');
-//         console.table(result);
-//         prompt();
-//     });
-// }
 
 function viewAllEmployees() {
     connection.query("SELECT employee.id, employee.first_name, employee.last_name, emprole.title, department.department_name AS department, emprole.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN employee AS manager on manager.id = employee.manager_id RIGHT JOIN emprole ON (emprole.id = employee.role_id) INNER JOIN department ON (department.id = emprole.department_id) ORDER BY employee.id;",
+        function (err, result) {
+            if (err) throw err;
+            console.table(result);
+            startApp();
+        })
+}
+
+function viewAllDepartments() {
+    connection.query("SELECT id, department_name FROM department ORDER by id",
         function (err, result) {
             if (err) throw err;
             console.table(result);
@@ -126,6 +105,15 @@ function viewByManager() {
 }
 
 function viewAllRoles() {
+    connection.query("SELECT emprole.id, emprole.title, emprole.salary, department.department_name FROM emprole JOIN department ON emprole.department_id = department.id ORDER by emprole.id",
+        function (err, result) {
+            if (err) throw err;
+            console.table(result);
+            startApp();
+        })
+}
+
+function viewByRole() {
     connection.query(`SELECT emprole.title, employee.id, employee.first_name, employee.last_name, department.department_name AS department
     FROM employee
     LEFT JOIN emprole ON (emprole.id = employee.role_id)
@@ -207,11 +195,11 @@ async function addEmployee() {
 }
 
 async function addARole() {
-    const addRole = await inquirer.prompt(askRole());
-    connection.query(`SELECT emprole.id, emprole.title FROM emprole ORDER by emprole.id;`, async (err, res) => {
+    connection.query(`SELECT id, department_name FROM department;`, async (err, res) => {
+        const addRole = await inquirer.prompt(askRole());
         if (err) throw err;
         const {
-            role
+            department
         } = await inquirer.prompt([{
             name: 'department',
             type: 'list',
@@ -220,7 +208,7 @@ async function addARole() {
         }]);
         let deptId;
         for (const row of res) {
-            if (row.title === deptId) {
+            if (row.department_name === department) {
                 deptId = row.id;
                 continue;
             }
@@ -230,7 +218,7 @@ async function addARole() {
             'INSERT INTO emprole SET ?', {
                 title: addRole.title,
                 salary: addRole.salary,
-                department_id: addRole.deptId
+                department_id: deptId,
             },
             (err, res) => {
                 if (err) throw err;
@@ -242,34 +230,16 @@ async function addARole() {
 
 async function addDepartment() {
     const addADept = await inquirer.prompt(askDept());
-    connection.query(`SELECT department.id, department.department_name FROM department ORDER by department.id;`, async (err, res) => {
-        if (err) throw err;
-        // const {
-        //     dept
-        // } = await inquirer.prompt([{
-        //     name: 'department',
-        //     type: 'list',
-        //     choices: () => res.map(res => res.department_name),
-        //     message: 'What is the new department name?: '
-        // }]);
-        let deptId;
-        for (const row of res) {
-            if (row.title === deptId) {
-                deptId = row.id;
-                continue;
-            }
+    console.log('Department has been added. Please refer to view all departments table to verify...');
+    connection.query(
+        'INSERT INTO department SET ?', {
+            department_name: addADept.department_name,
+        },
+        (err, res) => {
+            if (err) throw err;
+            startApp();
         }
-        console.log('Department has been added. Please refer to view all departments table to verify...');
-        connection.query(
-            'INSERT INTO department SET ?', {
-                department_name: addADept.department_name,
-            },
-            (err, res) => {
-                if (err) throw err;
-                startApp();
-            }
-        );
-    });
+    );
 }
 
 // function remove(input) {
@@ -314,33 +284,58 @@ function askId() {
 }
 
 async function updateRole() {
-    const employeeId = await inquirer.prompt(askId());
-    connection.query('SELECT emprole.id, emprole.title FROM emprole ORDER BY emprole.id;', async (err, res) => {
+    connection.query(`SELECT * FROM employee`, async (err, res) => {
         if (err) throw err;
-        const {
-            role
+        let choices = res.map(res => `${res.first_name} ${res.last_name}`);
+        choices.push('none');
+        let {
+            employee
         } = await inquirer.prompt([{
-            name: 'role',
+            name: 'employee',
             type: 'list',
-            choices: () => res.map(res => res.title),
-            message: 'What is the employees new role? '
+            choices: choices,
+            message: 'Choose the employee: '
         }]);
-        let roleId;
-        for (const row of res) {
-            if (row.title === role) {
-                roleId = row.id;
+        let employeeId;
+        let employeeName;
+        for (const data of res) {
+            data.fullName = `${data.first_name} ${data.last_name}`;
+            if (data.fullName === employee) {
+                employeeId = data.id;
+                employeeName = data.fullName;
+                console.log(employeeId);
+                console.log(employeeName);
                 continue;
             }
         }
-        connection.query(`UPDATE employee
-        SET role_id = ${roleId}
-        WHERE employee.id = ${employeeId.name}`, async (err, res) => {
+        connection.query('SELECT emprole.id, emprole.title FROM emprole ORDER BY emprole.id;', async (err, res) => {
             if (err) throw err;
-            console.log('Role has been updated')
-            startApp();
+            const {
+                role
+            } = await inquirer.prompt([{
+                name: 'role',
+                type: 'list',
+                choices: () => res.map(res => res.title),
+                message: 'What is the employees new role? '
+            }]);
+            let roleId;
+            for (const row of res) {
+                if (row.title === role) {
+                    roleId = row.id;
+                    continue;
+                }
+            }
+            connection.query(`UPDATE employee
+        SET role_id = ${roleId}
+        WHERE employee.id = ${employeeId}`, async (err, res) => {
+                if (err) throw err;
+                console.log('Role has been updated')
+                startApp();
+            });
         });
-    });
+    })
 }
+
 
 function askName() {
     return ([{
@@ -356,23 +351,19 @@ function askName() {
 
 function askRole() {
     return ([{
-        name: "name",
+        name: "title",
         type: "input",
         message: "What is the role title?"
     }, {
         name: "salary",
         type: "input",
         message: "What is the annual salary for this role?"
-    }, {
-        name: "dept",
-        type: "input",
-        message: "To what department does this role belong?"
     }]);
 }
 
 function askDept() {
     return ([{
-        name: "name",
+        name: "department_name",
         type: "input",
         message: "What is the department name?"
     }]);
